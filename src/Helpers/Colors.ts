c80dc94f2273ac11c96701e3311c0f283d48199c
Sebastian Otalora MB-JSOB <jhonsebas77@outlook.com>
2018-09-24 00:03:31 -0500

    Try to change the color of backgroun depend on the type

---
+++ src/Helpers/Colors.ts
@@ -0,0 +1,33 @@
+export const Colors = {
+    normal: '#E3E1D0',
+    fight: '#7A0F0C',
+    flying: '#B3D3D3',
+    poison: '#78309F',
+    ground: '#6D3100',
+    rock: '#5D554E',
+    bug: '#AFD121',
+    ghost: '#502293',
+    steel: '#4c669f',
+    fire: '#3b5998',
+    water: '#192f6a',
+    grass: '#192f6a',
+    electric: '#192f6a',
+    psychic: '#192f6a',
+    ice: '#192f6a',
+    dragon: '#192f6a',
+    dark: '#192f6a',
+    fairy: '#4c669f',
+    unknown: '#192f6a',
+    shadow: '#192f6a'
+}
+
+export const ColorType = (type1, type2?) => {
+    let ColorsType = type1 + (type2 ? '_' + type2 : '')
+    const typeColor = {
+        'fire': () => (Colors.fire, Colors.fire),
+        'flying': () => (Colors.flying),
+        'water': () => (Colors.fire)
+    }
+    return typeColor[ColorsType]
+
+}
--- src/containers/pokemonDetail/content.tsx
+++ src/containers/pokemonDetail/content.tsx
@@ -3,9 +3,13 @@ import { Text, View, Image, ImageBackground, FlatList } from 'react-native'
 import { getURL } from '../../util/api'
 import { paddingNumber, getTypeSource, getNormalSpriteSource } from '../../Helpers/Validators'
 import _ from '../../Helpers/Utilities'
+import { Colors, ColorType } from '../../Helpers/Colors'
 import NavBarSimple from '../../components/NavBar/Simple'
 import styles from './style'
 import Chip from '../../components/Chip'
+import Loading from '../../components/Loading'
+import LinearGradient from 'react-native-linear-gradient'
+
 const dataChip = [
     {
         'gender': 'F',
@@ -34,19 +38,25 @@ const dataChip = [
     }
 ]
 export default class PokemonDetail extends Component<PkmnDetailProps, PkmnDetailState> {
+    TypeColor: any
     constructor(props) {
         super(props)
         this.state = {
-            pokemon: []
+            pokemon: [],
+            loaded: false
         }
         this.renderType = this.renderType.bind(this)
         this.renderSpritePokemon = this.renderSpritePokemon.bind(this)
+        // this.renderColorType = this.renderColorType.bind(this)
     }
 
     async componentWillMount() {
         let pokemonUrl = this.props.item.url
         let pokemon = await getURL(pokemonUrl)
-        this.setState({ pokemon })
+        console.log('====================================')
+        console.log('pokemon WillMount', pokemon)
+        console.log('====================================')
+        this.setState({ pokemon, loaded: true })
     }
 
     renderType(type) {
@@ -55,6 +65,12 @@ export default class PokemonDetail extends Component<PkmnDetailProps, PkmnDetail
             <View style={styles.typeContainer}>
                 <Image style={styles.type} source={{ uri: url }} />
             </View>
+        )
+    }
+
+    renderLoadingView() {
+        return (
+            <Loading imageLoading={require('../../Assets/images/BG_Loading.png')} textLoading={'Cargando el detalle'} />
         )
     }
 
@@ -77,10 +93,35 @@ export default class PokemonDetail extends Component<PkmnDetailProps, PkmnDetail
         )
     }
 
+    // renderColorType(type1, type2?) {
+    //     let ColorType
+    //     let ColorsType = type1 + (type2 ? '_' + type2 : '')
+    //     if (ColorsType === 'fire') {
+    //         ColorType = Colors.fire
+    //     } else if (ColorsType === 'poison_grass') {
+    //         ColorType = Colors.poison_grass
+    //     }
+
+    //     console.log('color', ColorType)
+
+    //     // let ColorsType = `Colors.` + type1 + (type2 ? '_' + type2 : '')
+    //     return ColorType
+    // }
+
     render() {
-        const { id, types, sprites, weight, height } = this.state.pokemon
+        const { id, types, sprites, weight, height, loaded } = this.state.pokemon
+        console.log('props', this.props)
+        // const colortype = this.renderColorType(types && types[0] && types[0].type.name, types && types[1] && types[1].type.name)
+        console.log('====================================')
+        const loquesea = ColorType(types && types[0] && types[0].type.name, types && types[1] && types[1].type.name)
+        console.log('state', this.state)
+        console.log('Value loquesea:', loquesea, 'Type Variable: ', typeof loquesea)
+        console.log('====================================')
+        if (!this.state.loaded) {
+            return this.renderLoadingView()
+        }
         return (
-            <ImageBackground source={require('../../Assets/images/BG_Loading.png')} style={styles.loading}>
+            <LinearGradient colors={[Colors.water, Colors.fight]} style={styles.loading} >
                 <NavBarSimple
                     icon={'back'}
                     contentCenter={this.renderMiddle()}
@@ -94,11 +135,11 @@ export default class PokemonDetail extends Component<PkmnDetailProps, PkmnDetail
                         }
                     </View>
                     <View style={{ flexDirection: 'row' }}>
-                        {types && types[0] ? this.renderType(types[0].type.name) : undefined}
                         {types && types[1] ? this.renderType(types[1].type.name) : undefined}
+                        {types && types[0] ? this.renderType(types[0].type.name) : undefined}
                     </View>
                 </View >
-                <View>
+                {/* <View>
                     <FlatList
                         horizontal={true}
                         showsHorizontalScrollIndicator={false}
@@ -120,8 +161,8 @@ export default class PokemonDetail extends Component<PkmnDetailProps, PkmnDetail
                     <Text style={{ color: 'white', textAlign: 'center', fontWeight: 'bold', paddingTop: 10 }}>
                         {'Informacion'}
                     </Text>
-                </View>
-            </ImageBackground >
+                </View> */}
+            </LinearGradient >
         )
     }
 }
