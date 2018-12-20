1dbcbf04d55c9b5525adecd920a56536adb11ada
Sebastian Otalora MB-JSOB <jhonsebas77@outlook.com>
2018-12-20 00:02:31 -0500

    [BC] Add new Library to helpers screen resolution

--- src/Helpers/Utilities.ts
+++ src/Helpers/Utilities.ts
@@ -4,6 +4,12 @@ export default {
     capitalize: (value: string, all?: boolean) => Sugar.String.capitalize(value, true, all),
     upper: (value: string) => value ? value.toUpperCase() : value,
     lower: (value: string) => value.toLowerCase(),
+    has: (value: Object, key: string) => Sugar.Object.has(value, key),
+    exclude: (value: Object, key: string) => Sugar.Object.reject(value, key),
+    isObject: (value: any) => Sugar.Object.isObject(value),
+    isNumber: (value: any) => Sugar.Object.isNumber(value),
+    objectEach: (value: Object, fcn) => Sugar.Object.forEach(value, fcn),
+    get: (value, key: string) => Sugar.Object.get(value, key),
     formatNumberDecimal: (value, decimals) => value.toFixed(decimals),
     findProperty: function findProperty(object, property) {
         let value
